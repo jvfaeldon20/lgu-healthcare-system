1b6a841aab5480aa1a968294059200144999b2fe
JV Faeldon <jonelfaeldon@gmail.com>
2023-03-05 22:46:24 +0800

    refactor dashboard page

--- dashboard.php
+++ dashboard.php
@@ -46,168 +46,183 @@
 
 		<div class="main-panel">
 			<div class="content">
-				
 				<div class="page-inner mt--2">
 					
-				<div class="alert alert-<?= $_SESSION['success']; ?> <?= $_SESSION['success']=='danger' ? 'bg-danger text-light' : null ?>" role="alert">
+					<!-- login alert -->
+					<?php if(isset($_SESSION['message'])): ?>
+						<div class="alert alert-<?= $_SESSION['success']; ?> <?= $_SESSION['success']=='danger' ? 'bg-danger text-light' : null ?>" role="alert">
 							<?php echo $_SESSION['message']; ?>
+						</div>
+					<?php unset($_SESSION['message']); ?>
+					<?php endif ?>
+					<!-- end of login alert -->
+					
+					
+					<!-- analytics -->
+					<?php if(isset($_SESSION['username']) && $_SESSION['role'] =='system-maintenance'):?>
+					
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card card-stats card-primary card-round">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-3">
+												<div class="icon-big text-center">
+													<i class="flaticon-users"></i>
+												</div>
+											</div>
+											<div class="col-3 col-stats">
+											</div>
+											<div class="col-6 col-stats">
+												<div class="numbers mt-4">
+													<h2 class="fw-bold text-uppercase">Population</h2>
+													<h3 class="fw-bold text-uppercase"><?= number_format($total) ?></h3>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="card-body">
+										<a href="resident_info.php?state=all" class="card-link text-light">Total Population </a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card card-stats card-secondary card-round">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-3">
+												<div class="icon-big text-center">
+													<i class="flaticon-user"></i>
+												</div>
+											</div>
+											<div class="col-3 col-stats">
+											</div>
+											<div class="col-6 col-stats">
+												<div class="numbers mt-4">
+													<h2 class="fw-bold text-uppercase">Male</h2>
+													<h3 class="fw-bold"><?= number_format($male) ?></h3>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="card-body">
+										<a href="resident_info.php?state=male" class="card-link text-light">Total Male </a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card card-stats card-warning card-round">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-3">
+												<div class="icon-big text-center">
+													<i class="icon-user-female"></i>
+												</div>
+											</div>
+											<div class="col-3 col-stats">
+											</div>
+											<div class="col-6 col-stats">
+												<div class="numbers mt-4">
+													<h2 class="fw-bold text-uppercase">Female</h2>
+													<h3 class="fw-bold text-uppercase"><?= number_format($female) ?></h3>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="card-body">
+										<a href="resident_info.php?state=female" class="card-link text-light">Total Female </a>
+									</div>
+								</div>
+							</div>
 						</div>
 						
 						<div class="row">
-						<div class="col-md-4">
-							<div class="card card-stats card-primary card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-3">
-											<div class="icon-big text-center">
-												<i class="flaticon-users"></i>
+							<div class="col-md-4">
+								<div class="card card-stats card-success card-round">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-3">
+												<div class="icon-big text-center">
+													<i class="fas fa-fingerprint"></i>
+												</div>
+											
+											
 											</div>
-										</div>
-										<div class="col-3 col-stats">
-										</div>
-										<div class="col-6 col-stats">
-											<div class="numbers mt-4">
-												<h2 class="fw-bold text-uppercase">Population</h2>
-												<h3 class="fw-bold text-uppercase"><?= number_format($total) ?></h3>
+											<div class="col-3 col-stats">
 											</div>
-										</div>
-									</div>
-								</div>
-								<div class="card-body">
-									<a href="resident_info.php?state=all" class="card-link text-light">Total Population </a>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="card card-stats card-secondary card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-3">
-											<div class="icon-big text-center">
-												<i class="flaticon-user"></i>
-											</div>
-										</div>
-										<div class="col-3 col-stats">
-										</div>
-										<div class="col-6 col-stats">
-											<div class="numbers mt-4">
-												<h2 class="fw-bold text-uppercase">Male</h2>
-												<h3 class="fw-bold"><?= number_format($male) ?></h3>
+											<div class="col-6 col-stats">
+												<div class="numbers mt-4">
+													<h2 class="fw-bold text-uppercase">Purok</h2>
+													<h3 class="fw-bold text-uppercase"><?= number_format($purok) ?></h3>
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-								<div class="card-body">
-									<a href="resident_info.php?state=male" class="card-link text-light">Total Male </a>
+									<div class="card-body">
+										<a href="purok_info.php?state=purok" class="card-link text-light">Purok Information</a>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="card card-stats card-warning card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-3">
-											<div class="icon-big text-center">
-												<i class="icon-user-female"></i>
+							<div class="col-md-4">
+								<div class="card card-stats card-round card-danger">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-3">
+												<div class="icon-big text-center">
+													<i class="icon-layers"></i>
+												</div>
 											</div>
-										</div>
-										<div class="col-3 col-stats">
-										</div>
-										<div class="col-6 col-stats">
-											<div class="numbers mt-4">
-												<h2 class="fw-bold text-uppercase">Female</h2>
-												<h3 class="fw-bold text-uppercase"><?= number_format($female) ?></h3>
+											<div class="col-3 col-stats">
+											</div>
+											<div class="col-6 col-stats">
+												<div class="numbers mt-4">
+													<h2 class="fw-bold text-uppercase">Appointment</h2>
+													<h3 class="fw-bold text-uppercase"><?= number_format($blotter) ?></h3>
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-								<div class="card-body">
-									<a href="resident_info.php?state=female" class="card-link text-light">Total Female </a>
-								</div>
-							</div>
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="col-md-4">
-							<div class="card card-stats card-success card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-3">
-											<div class="icon-big text-center">
-												<i class="fas fa-fingerprint"></i>
-											</div>
-										
-										
-										</div>
-										<div class="col-3 col-stats">
-										</div>
-										<div class="col-6 col-stats">
-											<div class="numbers mt-4">
-												<h2 class="fw-bold text-uppercase">Purok</h2>
-												<h3 class="fw-bold text-uppercase"><?= number_format($purok) ?></h3>
-											</div>
-										</div>
+									<div class="card-body">
+										<a href="appointment.php" class="card-link text-light">Appointment Information</a>
 									</div>
 								</div>
-								<div class="card-body">
-									<a href="purok_info.php?state=purok" class="card-link text-light">Purok Information</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="card card-stats card-round card-danger">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-3">
-											<div class="icon-big text-center">
-												<i class="icon-layers"></i>
-											</div>
-										</div>
-										<div class="col-3 col-stats">
-										</div>
-										<div class="col-6 col-stats">
-											<div class="numbers mt-4">
-												<h2 class="fw-bold text-uppercase">Appointment</h2>
-												<h3 class="fw-bold text-uppercase"><?= number_format($blotter) ?></h3>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="card-body">
-									<a href="appointment.php" class="card-link text-light">Appointment Information</a>
-								</div>
-							</div>
-						
-					</div>				
+							
+						</div>				
 	
 					<?php endif ?>
-					
-					<div class="row">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-head-row">
-										<div class="card-title fw-bold">
-											<h1>ANNOUNCEMENT</h1>
+					<!-- end of analytics -->
+
+
+					<!-- announcement -->
+					<?php if(isset($_SESSION['username']) && ($_SESSION['role'] =='resident' || $_SESSION['role'] =='medical-admin')):?>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<div class="card-head-row">
+											<div class="card-title fw-bold">
+												<h1>ANNOUNCEMENT</h1>
+											</div>
 										</div>
 									</div>
-								</div>
-								<div class="card-body">
-									<p><?= !empty($db_txt) ? $db_txt : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim.' ?></p>
-									<div class="text-center">
-										<img class="img-fluid" src="<?= !empty($db_img) ? 'assets/uploads/'.$db_img : 'assets/img/bg-abstract.png' ?>" />
+									<div class="card-body">
+										<p><?= !empty($db_txt) ? $db_txt : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim.' ?></p>
+										<div class="text-center">
+											<img class="img-fluid" src="<?= !empty($db_img) ? 'assets/uploads/'.$db_img : 'assets/img/bg-abstract.png' ?>" />
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
+					<?php endif ?>
+					<!-- end of announcement -->
+
+
 				</div>
 			</div>
 			<!-- Main Footer -->
 			<?php include 'templates/main-footer.php' ?>
 			<!-- End Main Footer -->
-			
 		</div>
 		
 	</div>
