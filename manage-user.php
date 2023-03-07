9138f175e86aa797b72c11fe6db88f51b9d6bbdd
JV Faeldon <jonelfaeldon@gmail.com>
2023-03-07 10:54:37 +0800

    add page menu - manage user

--- manage-user.php
+++ manage-user.php
@@ -13,45 +13,25 @@
 <html lang="en">
 <head>
 	<?php include 'templates/header.php' ?>
-	<title>User Management -  Barangay Healthcare System</title>
+	<title>Manage User - Masili Health Service System</title>
 </head>
 <body>
 <?php include 'templates/loading_screen.php' ?>
 	<div class="wrapper">
-		<!-- Main Header -->
 		<?php include 'templates/main-header.php' ?>
-		<!-- End Main Header -->
-
-		<!-- Sidebar -->
 		<?php include 'templates/sidebar.php' ?>
-		<!-- End Sidebar -->
 
 		<div class="main-panel">
 			<div class="content">
-				<div class="panel-header bg-primary-gradient">
-					<div class="page-inner">
-						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-							<div>
-								<h2 class="text-white fw-bold">Settings</h2>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="page-inner">
 					<div class="row mt--2">
 						<div class="col-md-12">
-
-                            <?php if(isset($_SESSION['message'])): ?>
-                                <div class="alert alert-<?php echo $_SESSION['success']; ?> <?= $_SESSION['success']=='danger' ? 'bg-danger text-light' : null ?>" role="alert">
-                                    <?php echo $_SESSION['message']; ?>
-                                </div>
-                            <?php unset($_SESSION['message']); ?>
-                            <?php endif ?>
-
                             <div class="card">
 								<div class="card-header">
 									<div class="card-head-row">
-										<div class="card-title">User Management</div>
+										<div class="card-title text-primary">
+											<h1>MANAGE USER</h1>
+										</div>
 										<div class="card-tools">
 											<a href="#add" data-toggle="modal" class="btn btn-info btn-border btn-round btn-sm">
 												<i class="fa fa-plus"></i>
@@ -64,7 +44,7 @@
 									<div class="table-responsive">
 										<table class="table table-striped ">
 											<thead>
-												<tr>
+												<tr class="text-primary">
 													<th scope="col">No.</th>
 													<th scope="col">Username</th>
 													<th scope="col">User Type</th>
@@ -101,15 +81,6 @@
 													</tr>
 												<?php endif ?>
 											</tbody>
-											<tfoot>
-												<tr>
-													<th scope="col">No.</th>
-													<th scope="col">Username</th>
-													<th scope="col">User Type</th>
-													<th scope="col">Created At</th>
-													<th scope="col">Action</th>
-												</tr>
-											</tfoot>
 										</table>
 									</div>
 								</div>
