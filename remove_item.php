11398d10bfcf393e31571094e2ff1c01e1a94962
JV Faeldon <jonelfaeldon@gmail.com>
2023-03-07 10:56:26 +0800

    dynamic delete table item

---
+++ remove_item.php
@@ -0,0 +1,14 @@
+<?php include 'server/server.php' ?>
+<?php
+    $delID = $_GET['id'];
+    $table = $_GET['tbl'];
+    $query = "DELETE FROM $table WHERE id=$delID";
+    $result = $conn->query($query);
+    $_SESSION['message'] = 'Failed to remove item!';
+    $_SESSION['success'] = 'danger';
+    if($result){
+        $_SESSION['message'] = 'Successfully removed item!';
+        $_SESSION['success'] = 'success';
+    }
+    header('location: medicine.php');
+?>
