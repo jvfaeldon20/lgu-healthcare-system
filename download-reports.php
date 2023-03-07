9ff077a2b4ae3afea60218715b04a56bedf70558
JV Faeldon <jonelfaeldon@gmail.com>
2023-03-07 10:53:17 +0800

    add page menu - download reports

--- download-reports.php
+++ download-reports.php
@@ -13,51 +13,25 @@
 <html lang="en">
 <head>
 	<?php include 'templates/header.php' ?>
-	<title>Barangay Purok -  Barangay Management System</title>
+	<title>Reports - Masili Health Service System</title>
 </head>
 <body>
 <?php include 'templates/loading_screen.php' ?>
 	<div class="wrapper">
-		<!-- Main Header -->
 		<?php include 'templates/main-header.php' ?>
-		<!-- End Main Header -->
-
-		<!-- Sidebar -->
 		<?php include 'templates/sidebar.php' ?>
-		<!-- End Sidebar -->
 
 		<div class="main-panel">
 			<div class="content">
-				<div class="panel-header bg-primary-gradient">
-					<div class="page-inner">
-						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-							<div>
-								<h2 class="text-white fw-bold">Settings</h2>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="page-inner">
 					<div class="row mt--2">
 						<div class="col-md-12">
-
-                            <?php if(isset($_SESSION['message'])): ?>
-                                <div class="alert alert-<?php echo $_SESSION['success']; ?> <?= $_SESSION['success']=='danger' ? 'bg-danger text-light' : null ?>" role="alert">
-                                    <?php echo $_SESSION['message']; ?>
-                                </div>
-                            <?php unset($_SESSION['message']); ?>
-                            <?php endif ?>
-
                             <div class="card">
 								<div class="card-header">
 									<div class="card-head-row">
-										<div class="card-title">Barangay Purok</div>
-										<div class="card-tools">
-											<a href="#add" data-toggle="modal" class="btn btn-info btn-border btn-round btn-sm">
-												<i class="fa fa-plus"></i>
-												Purok
-											</a>
-										</div>
+										<div class="card-title text-primary">
+                                            <h1>REPORTS</h1>
+                                        </div>
 									</div>
 								</div>
 								<div class="card-body">
@@ -65,7 +39,6 @@
                                         <table class="table table-striped">
                                             <thead>
                                                 <tr>
-                                                    <th scope="col">No.</th>
                                                     <th scope="col">Purok</th>
                                                     <th scope="col">Details</th>
                                                     <th scope="col">Action</th>
@@ -97,14 +70,6 @@
                                                     </tr>
                                                 <?php endif ?>
                                             </tbody>
-                                            <tfoot>
-                                                <tr>
-                                                    <th scope="col">No.</th>
-                                                    <th scope="col">Purok</th>
-                                                    <th scope="col">Details</th>
-                                                    <th scope="col">Action</th>
-                                                </tr>
-                                            </tfoot>
                                         </table>
                                     </div>
 								</div>
